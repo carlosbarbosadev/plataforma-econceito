06a57a3734ee022a39d179883186b58d7db46fcd
carlosbarbosadev <cdembalagensconceito@gmail.com>
2025-05-14 15:19:30 -0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 
 const app = express();
 app.use(express.json()); // 🔥 Sem isso, o req.body vem vazio!
