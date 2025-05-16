15c80268ae1921226eaff04862d2208f38b6b1e3
carlosbarbosadev <cdembalagensconceito@gmail.com>
2025-05-16 11:27:39 -0300

    commit

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // ex: http://localhost:3039/api
+  baseURL: import.meta.env.VITE_API_BASE_URL, // ex: http://localhost:3001/api
   headers: { 'Content-Type': 'application/json' },
 });
 
