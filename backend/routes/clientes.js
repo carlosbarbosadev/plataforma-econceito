ff059918a17595486f1c85262122e43705b8d1da
carlosbarbosadev <cdembalagensconceito@gmail.com>
2025-05-16 15:28:26 -0300

    commit

--- backend/routes/clientes.js
+++ backend/routes/clientes.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
+const pool = require('../db');
 const { autenticarToken } = require('../middlewares/authMiddleware');
 
 // Rota protegida com autenticação
-router.get('/', autenticarToken, (req, res) => {
-    const todosClientes = JSON.parse(fs.readFileSync('clientes.json', 'utf-8'));
+router.get('/', autenticarToken, async (req, res) => {
+  try {
+    // busca todos os clientes do banco 
+    const { rows: todosClientes } = await pool.query(`
+      SELECT id, nome, email, vendedor
+      FROM clientes
+      ORDER BY id
+    `);
 
     if (req.usuario.tipo === 'admin') {
-    // Admin vê todos
-    return res.json(todosClientes);
-    } else if (req.usuario.tipo === 'vendedor') {
-    // Vendedor vê  apenas seus clientes
-    const clienteDoVendedor = todosClientes.filter(
-        (cliente) => cliente.vendedor === req.usuario.email
-    );
-    return res.json(clienteDoVendedor);
-  } else {
-    return res.status(403).json({ mensagem: 'Tipo de usuário Inválido' });
+      // Admin vê todos
+      return res.json(todosClientes);
     }
-});
 
-module.exports = router;
+    if (req.usuario.tipo === 'vendedor') {
+      // Venedor só vê seus clientes
+      const meusClientes = todosClientes.filter(
+        (c) => c.vendedor === req.usuario.email
+      );
+      return res.json(meusClientes);
+    }
+
+    return res.status(403).json({ mensagem: 'Tipo de usuário inválido' });
+  } catch (err) {
+    console.error('Erro ao buscar clientes:', err);
+    return res.status(500).json({ mensagem: 'Erro interno do servidor' });
+  }
+});
