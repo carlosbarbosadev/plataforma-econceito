1ac7ada05be8c5aabea55ed7b59d25e444902c20
carlosbarbosadev <cdembalagensconceito@gmail.com>
2025-05-21 10:07:46 -0300

    commit

--- backend/routes/clientes.js
+++ backend/routes/clientes.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { autenticarToken } = require('../middlewares/authMiddleware');
+// const { autenticarToken } = require('../middlewares/authMiddleware');
 const { fetchClientes } = require('../services/bling');
 
-// Rota protegida com autenticação
-router.get('/', autenticarToken, async (req, res) => {
-  if (!req.usuario) {
-    return res.status(401).json({ mensagem: 'Usuário não autenticado' });
-  }
+// Rota SEM autenticação para teste
+router.get('/', /* autenticarToken,*/ async (req, res) => {
+
+  // LINHA ABAIXO COMENTADA PARA TESTE
+  //if (!req.usuario) {
+    //return res.status(401).json({ mensagem: 'Usuário não autenticado' });
+  //}
+
   try {
-    // traz do Bling
     const todosClientes = await fetchClientes();
 
-    if (req.usuario.tipo === 'admin') {
+/*    if (req.usuario.tipo === 'admin') {
       return res.json(todosClientes);
     }
     // só clientes do próprio vendedor
     const meus = todosClientes.filter(
       c => c.emailVendedor === req.usuario.email
     );
-    return res.json(meus);
+    return res.json(meus); */
+
+    // PARA TESTE, SIMPLESMTENTE RETORNE TODOS OS CLIENTES
+    return res.json(todosClientes);
     
   } catch (err) {
     console.error('Falha ao buscar dados do Bling:', err);
