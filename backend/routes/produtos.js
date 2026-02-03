5f6168aebfad8e8fd31b852d97b44e7714f63fd8
carlosbarbosadev <cdembalagensconceito@gmail.com>
2026-02-03 16:23:54 -0300

    chore(products-api): remove an unnecessary import0

--- backend/routes/produtos.js
+++ backend/routes/produtos.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { autenticarToken } = require('../middlewares/authMiddleware');
 const db = require('../db');
-const { sincronizarProdutos } = require('../services/blingSyncService');
 
 async function getIdsProdutosEmCampanhaAtiva() {
   try {
